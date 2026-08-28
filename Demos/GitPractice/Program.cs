9eec314871a6c01e577c835f2361abe5f46d116b
Erin Cascioli <edcigm@rit.edu>
2026-08-28 16:33:55 -0400

    Another commit of my gameplay stuff

--- Demos/GitPractice/Program.cs
+++ Demos/GitPractice/Program.cs
@@ -7,6 +7,10 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hi everyone! This will appear as a change" +
                 "inside Github Desktop.");
+            Console.WriteLine("Some more work");
+            Console.WriteLine("Pretend this is way more code than I'm typing here");
+            Console.WriteLine("Jumping for my character");
+            Console.WriteLine("Collision detection is here");
         }
     }
 }
