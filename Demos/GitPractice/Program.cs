21efdafdbe8be9278eae09ab7b57cd5829d2d5e6
Erin Cascioli <edcigm@rit.edu>
2026-08-28 16:32:59 -0400

    Did so much work here

--- Demos/GitPractice/Program.cs
+++ Demos/GitPractice/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi everyone! This will appear as a change" +
+                "inside Github Desktop.");
         }
     }
 }
