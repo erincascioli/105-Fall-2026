59af7575440850e76f27234aea9524d9ca58f917
Erin Cascioli <edcigm@rit.edu>
2026-08-31 16:37:01 -0400

    Practice with comments and multi-line statements

---
+++ Demos/CommentPractice2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Name
+// Date
+// Program purpose
+
+// Erin Cascioli
+// 8/31/26
+// Demo comments
+
+/* Name
+ * Date
+ * Purpose 
+ */
+
+
+namespace CommentPractice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcome the user to the game
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi, how are you today?");
+
+            // Explain the introductory room to the user
+            Console.WriteLine("kfgjbsjgb fiuznsd,vm zrskjtn a,mzsg kjv zs,mzr gvmznxbgjkersg b,mf " +
+                "gksj zdfm,g sk,mfbgmjsg ,vmc ykj d,gfbnjkr,gfb,vjc bmsrevfdvckjg f,mvc  ,mg fdbvc " +
+                "mjg,fbkcjv g,xbngktjeshrgkd,.vzmxbmjes5btd,mfbstkj b.,mhg kjstdgfxb mxdtg b,vmc " +
+                "mjxdt bmxgbmxjd bmn");
+
+            // Gather user's choice for the starting area of the game
+            Console.WriteLine("What do you want to do?");
+
+            Console.WriteLine("fhdkzghdx");                 // 6
+            Console.WriteLine("erakgsuheiulrgh");           // 17
+            Console.WriteLine("dsgzdghxdcf");               // 285
+        }
+    }
+}
