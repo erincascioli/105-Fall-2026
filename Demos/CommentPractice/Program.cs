46d6cd6cd1ccb20a76c96efd605314bfe985045a
Erin Cascioli <edcigm@rit.edu>
2026-08-31 14:45:39 -0400

    Practice writing comments and working with C.WL and C.W

---
+++ Demos/CommentPractice/Program.cs
@@ -0,0 +1,33 @@
+﻿// Name
+// Date
+// Program purpose
+
+/* Name
+ * Date
+ * Program purpose
+ * */
+
+
+
+namespace CommentPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display (print) my name to the window
+            Console.WriteLine("Erin Cascioli");
+
+            // Telling my students
+            Console.WriteLine("This sentence will appear on the line below my name Erin Cascioli");
+            Console.WriteLine("Howdy everyone it's monday i hope your week was fine and I hope " +
+                "your weekend will be great too and that you have lots of fun and also fit in " +
+                "homework");
+
+            // Demonstrating what Write looks like vs. WriteLine
+            Console.Write("Hi ");
+            Console.Write("there ");
+            Console.Write("everyone");
+        }
+    }
+}
