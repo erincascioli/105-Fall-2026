7e8e1aede6d0baa2b9e053a5888b5a3b11d82e86
Erin Cascioli <edcigm@rit.edu>
2026-09-02 16:21:09 -0400

    Updated Statements and Expressions demo

--- Demos/StatementsExpressions_Demo/Program.cs
+++ Demos/StatementsExpressions_Demo/Program.cs
@@ -9,18 +9,37 @@ namespace StatementsExpressions_Demo
     {
         static void Main(string[] args)
         {
-            // Demo statements and expressions
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("hey");
+            // An expression by itself won't compile
+            // All three of these cause syntax errors and have red squiggles underneath:
+            //"Hello everyone";
+            //4 + 274;
+            //12345;
 
-            // Simple math outputted to the console
-            Console.WriteLine(365 * 24);
-         
-            // Math concatenated with a string
-            Console.WriteLine("The answer is {0}" + 10/5);
+            // An expression must be in the greater context of a statement
+            //   in order to compile and execute. 
+            Console.WriteLine("Hello everyone");
+            Console.WriteLine(4 + 274);
+            Console.WriteLine(12345);
 
-            // Using the special placeholder with an argument list (advanced)
-            Console.WriteLine("I want to insert a value {0}", "here");
+            // Watch for integer vs. floating point division!   // Console shows:
+            Console.WriteLine("9/4 is " + (9/4));               // 2
+            Console.WriteLine("9.0/4 is " + (9.0/4));           // 2.25
+            Console.WriteLine("9.0/4.0 is " + (9.0/4.0));       // 2.25
+            Console.WriteLine("9/4.0 is " + (9/4.0));           // 2.25
+
+            // Mod gives us the remainder
+            Console.WriteLine("9%4 is " + (9%4));
+            
+            // Escape characters for quotations
+            Console.WriteLine("Then he said, \"Howdy!\"");
+            Console.WriteLine("Then he said, 'Howdy!'");
+            Console.WriteLine("Then he said, \'Howdy!\'");
+            Console.WriteLine("\\\"");
+
+            Console.WriteLine("5 plus 5 is " + (5 + 5));        // 10
+            Console.WriteLine("5 plus 5 is " + 5 + 5);          // 55
+            Console.WriteLine("5 times 5 is " + 5 + 5*2);       // 510
+
         }
     }
 }
