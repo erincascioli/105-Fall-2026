068e5d79aaee181379cb6e29f1a7f5f96ae5c800
Erin Cascioli <edcigm@rit.edu>
2026-09-21 16:52:19 -0400

    Updating debug code to include choice program (fries or milk)

--- Demos/DebugPractice2/Program.cs
+++ Demos/DebugPractice2/Program.cs
@@ -4,6 +4,33 @@
     {
         static void Main(string[] args)
         {
+            string choice1 = "sbxdjbxdj";
+            string phrase = "";
+
+            if( choice1 == "A" )
+            {
+                phrase = "Yay! A!";
+            }
+            else if(choice1 == "B")
+            {
+                phrase = "No! B!";
+            }
+            else
+            {
+                phrase = "Anything but A!";
+            }
+
+            //if (choice1 == "B")
+            //{
+            //    phrase = "No! B!";
+            //}
+
+            Console.WriteLine(phrase);
+
+            // WE WILL PICK UP WITH THIS PROGRAM IDEA ON WEDNESDAY!
+            // OPTIONS: FRENCH FRIES OR CURDLED MILK
+
+            /*
             // Declare 2 variables of whatever type with whatever value you want
             int number = 100;
             string word = "popcorn";
@@ -17,6 +44,7 @@
             Console.WriteLine(word);
             Console.WriteLine("rgiuhskgbjgseuldbfjvzfbvzjd");
             Console.WriteLine("kbdjhrjhbsjhb");
+            */
         }
     }
 }
