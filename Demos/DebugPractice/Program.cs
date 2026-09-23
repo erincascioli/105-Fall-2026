56da345ffcabec3cebe7f0ead7c389f11f3751bc
Erin Cascioli <edcigm@rit.edu>
2026-09-23 13:48:20 -0400

    Pushing updated code for nested if statements

--- Demos/DebugPractice/Program.cs
+++ Demos/DebugPractice/Program.cs
@@ -12,7 +12,69 @@ namespace DebugPractice
     {
         static void Main(string[] args)
         {
+            // TODO: Write pseudocode for driving ages of license and permit for NC and WY
 
+            // Nested if statement
+            // Driving age NC
+            // 15 --> Permit
+            // 16 --> Full license
+            // Driving age WY
+            // 12 --> Permit
+            // 15 --> License
+
+            // IF Live in NC THEN
+            //    IF 15 THEN
+            //        Permit
+            //    IF 16 THEN
+            //        License
+            // ELSE IF live in WY THEN
+            //    IF 12 THEN
+            //        Permit
+            //    IF 15 THEN
+            //        License
+            // ELSE
+            //    I don't know
+
+            string state = "NC";
+            int age = -4;
+
+            // TODO: Write a nested conditional for driving ages
+            if(state == "NC")
+            { 
+                if(age < 15)
+                {
+                    Console.WriteLine("You're too young to drive.");
+                }
+                else if(age == 15)
+                {
+                    Console.WriteLine("Permit");
+                }
+                else if(age == 16)
+                {
+                    Console.WriteLine("License");
+                }
+                else
+                {
+                    Console.WriteLine("You're older than 16 - You probably have a license?");
+                }
+            }
+            else if(state == "WY")
+            {
+                if (age == 12)
+                {
+                    Console.WriteLine("Permit");
+                }
+                else if (age == 15)
+                {
+                    Console.WriteLine("License");
+                }
+            }
+            else
+            {
+                Console.WriteLine("I don't know that state's laws");
+            }
+
+            /*
             // Apples are good
             // Live cockroaches are bad
 
@@ -26,12 +88,31 @@ namespace DebugPractice
             // Prompt user for their choice (C.RL)
             // Save answer in string variable answer
             // IF answer is apples THEN
-            //    Display ""
+            //    Display "Yummy"
+            // ELSE IF answer is cockroach THEN
+            //    Display "Gross"
             // ELSE
-            //    Display ""
+            //    Display "Don't recognize"
 
             // ** WE WILL PICK BACK UP FROM HERE ON WEDNESDAY'S CLASS! **
 
+            Console.WriteLine("Would you rather eat an APPLE or a LIVE COCKROACH?");
+            Console.Write("Enter either the word \"APPLE\" or the word \"COCKROACH\": ");
+            string answer = Console.ReadLine()!.Trim().ToUpper();
+
+            if(answer == "APPLE")
+            {
+                Console.WriteLine("Yummy!");
+            }
+            else if(answer == "COCKROACH")
+            {
+                Console.WriteLine("Gross!");
+            }
+            else
+            {
+                Console.WriteLine("I don't recognize that input.");
+            }
+            */
 
             #region Commented code
             // ----------------------------------------------------------------
